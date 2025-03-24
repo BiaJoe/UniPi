4e78b58aaa59a61bcab7bdecc27bf7d9bbb8300a
Biagio <biagiomontelisciani@gmail.com>
2025-03-24 16:51:23 +0100

    capitoli 1 e 2 di Verilog bombZ

--- AESO/verilog/basics/and_gate.v
+++
@@ -1,9 +0,0 @@
-// and_gate.v
-module and_gate(
-    input a, b,    // Inputs to the AND gate
-    output c       // Output from the AND gate
-);
-    assign c = a & b;  // AND operation
-endmodule
-
-
